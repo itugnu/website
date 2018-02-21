1fba57d5e5723d7e57317ffb8fc06fc5fe62a2fc
Emin Mastizada <emin@mastizada.com>
2018-02-21 21:53:37 +0300

    Added more controls to registering for lectures

--- web/components/lecture.py
+++ web/components/lecture.py
@@ -16,15 +16,18 @@ def lectures_list(request):
     return render(request, "lectures/index.html", data)
 
 
-@login_required
 def lectures_register(request):
     if request.method == 'GET':
         return JsonResponse({"message": _("Method not allowed")}, status=405)
+    if not request.user.is_authenticated:
+        return JsonResponse({"message": _("Please, login to register for lectures")}, status 403)
     lecture_pk = request.POST.get('lecture')
     try:
         lecture = Lecture.objects.get(pk=lecture_pk)
     except Lecture.DoesNotExist:
         return JsonResponse({"message": _("Lecture {pk} does not exist!").format(pk=lecture_pk)}, status=404)
+    if not lecture.is_registration_open:
+        return JsonResponse({"message": _("Lecture is not open for registration")}, status=403)
     if LectureApplication.objects.filter(lecture=lecture, user=request.user).exists():
         return JsonResponse({"message": _("You already registered for this lecture!")}, status=405)
     LectureApplication.objects.create(
